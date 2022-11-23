140b5949f4dbde18b96ee11ae60358e6a947c2db
Ranorien <ranorien@gmail.com>
2022-11-23 13:01:15 +0300

    New version

--- web_notepad/settings.py
+++ web_notepad/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
+# VENV variables loading from the file .env
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#fu=8mb1)y&jfwkek(8z-_5lj=k@+o#uf(%6(dt%&pak8_wc=_'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if os.environ.get('DEBUG') == 'FALSE':
+if env('DEBUG') == 'FALSE':
     DEBUG = False
 else:
     DEBUG = True
